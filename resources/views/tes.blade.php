ae34153732e8e408a4b1525ead53b7723b6f4568
fajarrama1210 <jarrr1210@gmail.com>
2024-04-03 09:48:33 +0700

    index.html

--- resources/views/tes.blade.php
+++ resources/views/tes.blade.php
@@ -16,16 +16,16 @@
     <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600&family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500&display=swap" rel="stylesheet">
     <!-- CSS here -->
     <link rel="stylesheet" href="{{ asset('./assets/css/animate.min.css')  }}">
-    <link rel="stylesheet" href="{{ asset('') }}./assets/css/magnific-popup.css">
-    <link rel="stylesheet" href="./assets/css/fontawesome-all.min.css">
-    <link rel="stylesheet" href="./assets/css/swiper-bundle.css">
-    <link rel="stylesheet" href="./assets/css/tg-cursor.css">
-    <link rel="stylesheet" href="./assets/css/jquery-ui.css">
-    <link rel="stylesheet" href="./assets/css/odometer.css">
-    <link rel="stylesheet" href="./assets/css/slick.css">
-    <link rel="stylesheet" href="./assets/css/jquery-ui.css">
-    <link rel="stylesheet" href="./assets/css/flaticon.css">
-    <link rel="stylesheet" href="./assets/css/flowbite.min.css">
+    <link rel="stylesheet" href="{{ asset('./assets/css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ asset('') }}./assets/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="{{ asset('') }}./assets/css/swiper-bundle.css">
+    <link rel="stylesheet" href="{{ asset('') }}./assets/css/tg-cursor.css">
+    <link rel="stylesheet" href="{{ asset('') }}./assets/css/jquery-ui.css">
+    <link rel="stylesheet" href="{{ asset('') }}./assets/css/odometer.css">
+    <link rel="stylesheet" href="{{ asset('') }}./assets/css/slick.css">
+    <link rel="stylesheet" href="{{ asset('') }}./assets/css/jquery-ui.css">
+    <link rel="stylesheet" href="{{ asset('') }}./assets/css/flaticon.css">
+    <link rel="stylesheet" href="{{ asset('') }}./assets/css/flowbite.min.css">
 	<link rel="stylesheet" type="text/css" href="./style.css">
 
     <!-- Page-Revealer -->
