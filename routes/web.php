c495c262321b6db805915605f64c91c1910b1f52
nugiartika <141117388+nugiartika@users.noreply.github.com>
2024-04-03 09:44:14 +0700

    laravel ui

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TesController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,4 +21,9 @@ Route::get('/', function () {
 
 Route::resource('tes', TesController::class);
 
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
