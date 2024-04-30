4dbd21be377058458d4485bf5f6fb6f404c40008
fajarrama1210 <jarrr1210@gmail.com>
2024-04-30 09:42:38 +0700

    svg

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -84,7 +84,7 @@
                             <div class="header-profile pointer">
                                 <div class="profile-wrapper d-flex align-items-center gap-3">
                                     <div class="img-area overflow-hidden">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" viewBox="0 0 36 36"><path fill="currentColor" d="M30.61 24.52a17.16 17.16 0 0 0-25.22 0a1.51 1.51 0 0 0-.39 1v6A1.5 1.5 0 0 0 6.5 33h23a1.5 1.5 0 0 0 1.5-1.5v-6a1.51 1.51 0 0 0-.39-.98" class="clr-i-solid clr-i-solid-path-1"/><circle cx="18" cy="10" r="7" fill="currentColor" class="clr-i-solid clr-i-solid-path-2"/><path fill="none" d="M0 0h36v36H0z"/></svg>                                    </div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 36 36"><path fill="currentColor" d="M30.61 24.52a17.16 17.16 0 0 0-25.22 0a1.51 1.51 0 0 0-.39 1v6A1.5 1.5 0 0 0 6.5 33h23a1.5 1.5 0 0 0 1.5-1.5v-6a1.51 1.51 0 0 0-.39-.98" class="clr-i-solid clr-i-solid-path-1"/><circle cx="18" cy="10" r="7" fill="currentColor" class="clr-i-solid clr-i-solid-path-2"/><path fill="none" d="M0 0h36v36H0z"/></svg>                                    </div>
                                     <span
                                         class="user-name d-none d-xxl-block text-nowrap">{{ auth()->user()->name }}</span>
                                     <i class="ti ti-chevron-down d-none d-xxl-block"></i>
