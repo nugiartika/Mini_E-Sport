6f56a1617e6ee0d7353ed149fa344da7d0a079dc
Amir Zuhdi Wibowo <cakadi190@gmail.com>
2024-05-18 20:04:26 +0700

    Fix: UI on Tournament Organizer list [Undone]

--- resources/views/penyelenggara/tournament.blade.php
+++ resources/views/penyelenggara/tournament.blade.php
@@ -1,7 +1,7 @@
-@extends('penyelenggara.layouts.app')
+@extends('layouts.panel')
 
 @section('content')
-    <div class="modal" tabindex="-1" id="filter" style="color: #000;">
+    {{-- <div class="modal" tabindex="-1" id="filter" style="color: #000;">
         <div class="modal-dialog modal-dialog-centered modal-dialog-split">
             <div class="modal-content">
                 <div class="modal-header">
@@ -211,5 +211,47 @@
                 }
             });
         }
-    </script>
+    </script> --}}
+
+    <div class="d-flex gap-2 mb-3 align-items-center justify-content-between">
+        <div>
+            <h4 class="h4 mb-1">Daftar Turnamen</h4>
+            <p class="text-muted mb-0">Daftar turnamen yang sudah ditambahkan</p>
+        </div>
+
+        <div>
+            <a href="" class="btn btn-primary">Buat Turnamen Baru</a>
+        </div>
+    </div>
+
+    <div class="row">
+        @forelse ($tournaments as $tournament)
+            <div class="col-md-4 col-lg-3 mb-3">
+                <div class="card h-100">
+                    <div class="overflow-hidden w-100" style="height: 200px">
+                        <img class="w-100 h-100" src="{{ asset("storage/{$cat->photo}") }}" alt="game"
+                            style="object-fit: cover;" />
+                    </div>
+                    <div class="card-body">
+                        <div class="pb-3 border-bottom mb-3 d-flex justify-content-between">
+                            <h5 class="mb-0">Nama Game</h5>
+                            <span>{{ $cat->name }}</span>
+                        </div>
+                        <div class="pb-2 d-flex justify-content-between">
+                            <h5 class="mb-0">Anggota per Tim</h5>
+                            <span>{{ $cat->membersPerTeam }} orang / tim</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @empty
+            <div class="col-12 d-flex flex-column justify-content-center">
+                <img src="{{ asset('assets/img/No-data.png') }}" alt=""
+                    style="display: block; margin: 0 auto; max-width: 16%; height: auto;">
+                <h4 class="table-light" style="text-align: center;">
+                    Data Turnamen Tidak Tersedia
+                </h4>
+            </div>
+        @endforelse
+    </div>
 @endsection
