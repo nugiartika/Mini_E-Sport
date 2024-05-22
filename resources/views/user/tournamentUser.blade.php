221a752b3d3f5575fd837dd1014a4dca04cfe5e1
nasyasrv <163367751+nasyasrv@users.noreply.github.com>
2024-05-22 14:46:14 +0700

    detail

--- resources/views/user/tournamentUser.blade.php
+++ resources/views/user/tournamentUser.blade.php
@@ -253,8 +253,8 @@
                                         @elseif ($totalTeams && $totalTeams == $tournament->slotTeam)
                                         @endif
 
-                                        <!-- Tombol Detail Tournament -->
-                                        <a href="{{ route('tournament.detail', $tournament->id) }}"
+                                        <!-- Tombol Detail Tournament --> 
+                                        <a href="{{ route('tournament.detailUser') }}"
                                             class="custom-icon-detail" data-bs-toggle="tooltip" data-bs-placement="top"
                                             style="display: flex; justify-content: center; align-items: center;"
                                             title="Detail Tournament">
