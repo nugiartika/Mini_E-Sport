41d1cb3720f906e65fe85e5dc3f80c8f4f557d03
nasyasrv <163367751+nasyasrv@users.noreply.github.com>
2024-05-30 09:20:16 +0700

    dashboard

--- resources/views/penyelenggara/Dashboard.blade.php
+++ resources/views/penyelenggara/Dashboard.blade.php
@@ -1,6 +1,80 @@
 @extends('layouts.panel')
 
 @section('content')
+    <div class="card-widget-separator-wrapper">
+        <div class="card-body card-widget-separator">
+            <div class="row gy-4 gy-sm-1 " >
+                <div class="col-sm-6 col-lg-4">
+                    <div
+                        class="d-flex justify-content-between align-items-start card-widget-1 border-end pb-3 pb-sm-0">
+                        <div>
+                            <h6 class="mb-2">JUMLAH TOURNAMENT DITERIMA</h6>
+                            @foreach ($tournaments as $tournament )
+                            <h4 class="mb-2">{{$tournament->status == 'accepted'}}</h4>
+                            @endforeach
+
+                            <p class="mb-0"><span class="text-muted me-2">Humma Esport</span></p>
+                        </div>
+                        <span class="avatar me-sm-4">
+                            <span class="avatar-initial bg-label-secondary rounded"><i
+                                    class="fas fa-trophy"></i></span>
+                        </span>
+                    </div>
+                    <hr class="d-none d-sm-block d-lg-none me-4">
+                </div>
+                <div class="col-sm-6 col-lg-4">
+                    <div
+                        class="d-flex justify-content-between align-items-start card-widget-1 border-end pb-3 pb-sm-0">
+                        <div>
+                            <h6 class="mb-2">JUMLAH TOURNAMENT DITOLAK</h6>
+                            @foreach ($tournaments as $tournament )
+                            <h4 class="mb-2">{{$tournament->status == 'rejected'}}</h4>
+                            @endforeach
+                            <p class="mb-0"><span class="text-muted me-2">Humma Esport</span></p>
+                        </div>
+                        <span class="avatar me-sm-4">
+                            <span class="avatar-initial bg-label-secondary rounded"><i
+                                    class="fas fa-trophy"></i></span>
+                        </span>
+                    </div>
+                    <hr class="d-none d-sm-block d-lg-none me-4">
+                </div>
+                <div class="col-sm-6 col-lg-4">
+                    <div
+                        class="d-flex justify-content-between align-items-start card-widget-1 border-end pb-3 pb-sm-0">
+                        <div>
+                            <h6 class="mb-2">JUMLAH TOURNAMENT PENDING</h6>
+                            @foreach ($tournaments as $tournament )
+                            <h4 class="mb-2">{{$tournament->status == 'pending'}}</h4>
+                            @endforeach
+                            <p class="mb-0"><span class="text-muted me-2">Humma Esport</span></p>
+                        </div>
+                        <span class="avatar me-sm-4">
+                            <span class="avatar-initial bg-label-secondary rounded"><i
+                                    class="fas fa-trophy"></i></span>
+                        </span>
+                    </div>
+                    <hr class="d-none d-sm-block d-lg-none me-4">
+                </div>
+                <div class="col-sm-6 col-lg-4 mt-4">
+                    <div
+                        class="d-flex justify-content-between align-items-start card-widget-2 ">
+                        <div>
+                            <h6 class="mb-2">SALDO PENYELENGGARA</h6>
+                            <h4 class="mb-2">Rp. {{ number_format($tournament->nominal, 0, '.', ',') }}</h4>
+                            <p class="mb-0"><span class="text-muted me-2">Humma Esport</span></p>
+                        </div>
+                        <span class="avatar p-2 me-lg-4">
+                            <span class="avatar-initial bg-label-secondary rounded"><i
+                                    class="fa fa-dollar"></i></span>
+                        </span>
+                    </div>
+                    <hr class="d-none d-sm-block d-lg-none">
+                </div>
+            </div>
+        </div>
+    </div>
+<br><br>
     <h1>Daftar Turnamen</h1>
 
     <section class="swiper-3d">
