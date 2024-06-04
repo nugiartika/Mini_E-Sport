8baaa02fb7645a446d636575a24a80a74219ee95
Rafisyahhh <muhamadakbar1801@gmail.com>
2024-06-04 15:54:51 +0700

    logout landing page

--- resources/views/Landingpage/index.blade.php
+++ resources/views/Landingpage/index.blade.php
@@ -101,9 +101,7 @@
                             </div>
                         @endif
                     </div>
-
                 </div>
-
             </div>
         </div>
     </header>
@@ -270,7 +268,7 @@
                                     for="category{{ $game->id }}">
                                 {{ $game->name }}
                                 </label>
-                            </div>
+                            </div> 
                         @endforeach
                     </form>
                 </div>
