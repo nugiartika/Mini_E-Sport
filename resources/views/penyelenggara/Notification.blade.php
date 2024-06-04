58d69c1ff7517769162a76fb846eb6d204bc1993
nasyasrv <163367751+nasyasrv@users.noreply.github.com>
2024-06-04 14:10:24 +0700

    Update Notification.blade.php

--- resources/views/penyelenggara/Notification.blade.php
+++ resources/views/penyelenggara/Notification.blade.php
@@ -4,75 +4,63 @@
     <div class="col-12">
         @forelse ($tournaments as $tournament)
                 @if ($tournament->status == 'rejected')
-                    <div class="card  mt-3" >
-                        <div class="card-header">
-                            <div style="display: inline-block;">
-                                <img src="assets/img/humma-01.png" width="60" height="50" style="vertical-align: middle; margin-left:30px">
-                                <h6 style="display: inline-block; vertical-align: middle; margin-left: 5px;">Humma E-Sport</h6>
-                                <p class="ml-auto" style="display: inline-block; vertical-align: middle; margin-left: 725px;">{{ $tournament->updated_at->format('H:i') }}</p>
-                            </div>
-                        </div>
-
-                        <div class="card-body">
-                            <div class="d-flex flex-column align-items-start">
-                                <div class="mb-2 d-flex align-items-center" style="margin-left:35px">
-                                    <h5 class="card-title me-3">Nama Tournament :</h5>
-                                    <p class="card-text mb-3">{{ $tournament->name }}</p>
-                                </div>
-                                <div class="mb-2 d-flex align-items-center" style="margin-left:35px">
-                                    <h5 class="card-title me-3">Status :</h5>
-                                    @if($tournament->status == 'accepted')
-                                        <p class="card-text mb-3">Diterima</p>
-                                    @else
-                                        <p class="card-text mb-3">Ditolak</p>
+                    <div class="mt-3">
+                        <div class="card d-flex flex-row align-items-start">
+                            <img src="assets/img/humma-01.png" width="150" height="110" style="vertical-align: middle; margin-top:25px; margin-left:25px; ">
+                            <div class="card-body" style="margin-left: 20px; margin-top:10px;">
+                                <div class="d-flex flex-column align-items-start">
+                                    <div class="d-flex align-items-center">
+                                        <h5 class="card-title me-3">{{ $tournament->name }}</h5>
+                                        <p style="margin-left: 530px">{{ $tournament->updated_at->format('H:i') }}</p>
+                                    </div>
+                                    <div class="d-flex align-items-center">
+                                        <p class="card-text me-3">Status :</p>
+                                        @if($tournament->status == 'accepted')
+                                            <p class="card-text mb-3">Diterima</p>
+                                        @else
+                                            <p class="card-text mb-3">Ditolak</p>
+                                        @endif
+                                    </div>
+                                    <div class="d-flex align-items-center">
+                                        <p class="card-text me-3">Pesan :</p>
+                                        <p class="card-text mb-3">{{ $tournament->reason }}</p>
+                                    </div>
+                                    @if ($tournament->notif == 'belum baca')
+                                        <a href="{{ route('Updatenotification', ['id' => $tournament->id]) }}" class="btn btn-primary btn-sm ms-auto">Tandai sudah baca <i class="fa fa-check mx-2"></i></a>
                                     @endif
                                 </div>
-                                <div class="mb-3 d-flex align-items-center" style="margin-left:35px">
-                                    <h5 class="card-title me-3">Pesan :</h5>
-                                    <p class="card-text mb-3">{{ $tournament->reason }}</p>
-                                </div>
-                                @if ($tournament->notif == 'belum baca')
-                                    <a href="{{ route('Updatenotification', ['id' => $tournament->id]) }}" class="btn btn-primary btn-sm ml-auto" style="margin-left: 815px"> Tandai sudah baca <i class="fa fa-check mx-2"></i></a>
-                                @endif
                             </div>
                         </div>
-
                     </div>
                 @else
-                <div class="card  mt-3" >
-                    <div class="card-header">
-                        <div style="display: inline-block;">
-                            <img src="assets/img/humma-01.png" width="60" height="50" style="vertical-align: middle; margin-left:30px">
-                            <h6 style="display: inline-block; vertical-align: middle; margin-left: 5px;">Humma E-Sport</h6>
-                            <p class="ml-auto" style="display: inline-block; vertical-align: middle; margin-left: 725px;">{{ $tournament->updated_at->format('H:i') }}</p>
+                    <div class="mt-3">
+                        <div class="card d-flex flex-row align-items-start">
+                            <img src="assets/img/humma-01.png" width="150" height="110" style="vertical-align: middle; margin-top:25px; margin-left:25px;">
+                            <div class="card-body" style="margin-left: 20px; margin-top:10px;">
+                                <div class="d-flex flex-column align-items-start">
+                                    <div class="d-flex align-items-center">
+                                        <h5 class="card-title me-3">{{ $tournament->name }}</h5>
+                                        <p style="margin-left: 530px">{{ $tournament->updated_at->format('H:i') }}</p>
+                                    </div>
+                                    <div class="d-flex align-items-center">
+                                        <p class="card-text me-3">Status :</p>
+                                        @if($tournament->status == 'accepted')
+                                            <p class="card-text mb-3">Diterima</p>
+                                        @else
+                                            <p class="card-text mb-3">Ditolak</p>
+                                        @endif
+                                    </div>
+                                    <div class="d-flex align-items-center">
+                                        <p class="card-text me-3">Pesan :</p>
+                                        <p class="card-text mb-3">Tournament anda sudah kami setujui</p>
+                                    </div>
+                                    @if ($tournament->notif == 'belum baca')
+                                        <a href="{{ route('Updatenotification', ['id' => $tournament->id]) }}" class="btn btn-primary btn-sm ms-auto">Tandai sudah baca <i class="fa fa-check mx-2"></i></a>
+                                    @endif
+                                </div>
+                            </div>
                         </div>
                     </div>
-
-                    <div class="card-body">
-                        <div class="d-flex flex-column align-items-start">
-                            <div class="mb-2 d-flex align-items-center" style="margin-left:35px">
-                                <h5 class="card-title me-3">Nama Tournament :</h5>
-                                <p class="card-text mb-3">{{ $tournament->name }}</p>
-                            </div>
-                            <div class="mb-2 d-flex align-items-center" style="margin-left:35px">
-                                <h5 class="card-title me-3">Status :</h5>
-                                @if($tournament->status == 'accepted')
-                                    <p class="card-text mb-3">Diterima</p>
-                                @else
-                                    <p class="card-text mb-3">Ditolak</p>
-                                @endif
-                            </div>
-                            <div class="mb-3 d-flex align-items-center" style="margin-left:35px">
-                                <h5 class="card-title me-3">Pesan :</h5>
-                                <p class="card-text mb-3">Tournament anda sudah kami setujui</p>
-                            </div>
-                            @if ($tournament->notif == 'belum baca')
-                                <a href="{{ route('Updatenotification', ['id' => $tournament->id]) }}" class="btn btn-primary btn-sm ml-auto" style="margin-left: 815px"> Tandai sudah baca <i class="fa fa-check mx-2"></i></a>
-                            @endif
-                        </div>
-                    </div>
-
-                </div>
                 @endif
         @empty
             <div class="col-lg-12 mt-5">
